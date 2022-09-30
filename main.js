8db69de7a11a20634e75f737078185aba5aabf6d
magnus_quist <magnus.quist@lerniastudent.se>
2022-10-01 00:07:52 +0200

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 document.querySelector("#submitButton").addEventListener("click", sayGoodbye);
 
 function sayGoodbye() {
-    let foo = document.querySelector("h1");
-    foo.innerText = "Goodbye World";
-    foo.nextSibling.remove;
+    document.querySelector("h1").innerText = "Goodbye World";
+
 }
